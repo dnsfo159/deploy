777d5b3c69c029e09c71e41386f96ac7211afe51
dnsfo159 <dnsfo159@naver.com>
2021-03-17 14:15:33 +0900

    test appㅅㄷㄴㅅ

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!!
-        </a>
+        테스트입니다 . 빌드를 안해도되는건지 아니면 깃으로만 되는건지
       </header>
     </div>
   );
